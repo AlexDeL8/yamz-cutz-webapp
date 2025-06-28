8690bc54550a5a87a075d4b800f4df6a19b1cdcf
Alex De Luca <alexnaj88@gmail.com>
2025-06-28 14:50:09 -0400

    feat: initial App setup and project structure

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,8 @@
+<nav>
+  <ul class="navList">
+    <li class="navListItem">Home</li>
+    <li class="navListItem">Services & Work</li>
+    <li class="navListItem">About</li>
+  </ul>
+  <button class="bookingBtn">Book Now</button>
+</nav>
