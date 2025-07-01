31892488fdaee0973e5d83ea4a3a1162cc698e93
Alex De Luca <alexnaj88@gmail.com>
2025-07-01 14:55:22 -0400

    fix: removing unused svg

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-  import svelteLogo from "./assets/svelte.svg";
-  import viteLogo from "/vite.svg";
   import Navbar from "./lib/components/Navbar.svelte";
   import Footer from "./lib/components/Footer.svelte";
 </script>
